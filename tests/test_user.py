4f1129520c8772d377bcfe9a9e38e024e8db30cd
charles K <kcharles52@gmail.com>
2018-07-24 17:00:36 +0300

    Remove indetation

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,7 +5,7 @@ from app.views import app
 class UserTest(BaseTestCaseUser):
 
     #User registration tests
-     def test_register_user_without_data(self):
+    def test_register_user_without_data(self):
         """Function to test if a user can register without providing data"""
         response = self.test_client.post(
             '/api/v1/users', data=json.dumps({}), content_type='application/json')
