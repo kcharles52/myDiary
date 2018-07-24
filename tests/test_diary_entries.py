9a6de1c535b828c714e05b99036957925cd7646e
charles K <kcharles52@gmail.com>
2018-07-24 22:05:51 +0300

    Write test to check if user can create entry without data

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -5,12 +5,19 @@ from app.views import app
 
 class EntriesTest(BaseTestCaseDiaryEntry):
         #tests for creating diary entry
-    def test_create_request(self):
+    def test_create_entry(self):
         """ Tests whether a user can create an entry successfully """
         response = self.test_client.post(
             '/api/v1/entries', data=json.dumps(self.diary_entry_data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn("You have successfully created your entry",str(response.data))
+
+    def test_create_entry_without_data(self):
+        """ Tests whether a user can create an entry without data """
+        response = self.test_client.post(
+            '/api/v1/entries', data=json.dumps({}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Enter data in all fields",str(response.data))
 
     def test_create_entry_without_title(self):
         """ Tests whether a user can not create an entry without a title """
