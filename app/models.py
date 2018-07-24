ffb8cbe474a01d612ccc08d2f5dfec2904c2ae43
charles K <kcharles52@gmail.com>
2018-07-24 22:04:58 +0300

    create a class to model diary entries

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 #empty lists to store data
 users=[]
 diaryEntries=[]
+
+
+class DiaryEntry:
+    def __init__(self, diaryTitle, date, diaryEntryBody, entry_id):
+        self.diaryTitle = diaryTitle
+        self.diaryEntryBody = diaryEntryBody
+        self.date = date
+
+    def __repr__(self):
+        return repr(self.__dict__)
