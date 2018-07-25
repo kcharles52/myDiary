d304e700c5f4ba088619b19cb5af30d03ce05949
charles K <kcharles52@gmail.com>
2018-07-25 08:17:58 +0300

    change variables passed in test functions

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -22,20 +22,20 @@ class EntriesTest(BaseTestCaseDiaryEntry):
     def test_create_entry_without_title(self):
         """ Tests whether a user can not create an entry without a title """
         response = self.test_client.post('/api/v1/entries', data=json.dumps(
-            {"title": "", "date": "2/2/2017", "diaryBody": "Entry without title"}), content_type='application/json')
+            {"diaryTitle": "", "date": "2/2/2017", "diaryBody": "Entry without title"}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        self.assertIn('title is required', str(response.data))
+        self.assertIn('Title is required', str(response.data))
 
     def test_create_entry_without_date(self):
         """ Tests whether a user can not create an entry without a date """
         response = self.test_client.post('/api/v1/entries', data=json.dumps(
-            {"title": "Wed", "date": "", "diaryBody": "Entry without title"}), content_type='application/json')
+            {"diaryTitle": "Wed", "date": "", "diaryBody": "Entry without title"}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertIn('date is required', str(response.data))
 
     def test_create_entry_without_body(self):
         """ Tests whether a user can not create an entry without a body """
         response = self.test_client.post('/api/v1/entries', data=json.dumps(
-            {"title": "Wed", "date": "", "diaryBody": "Entry without title"}), content_type='application/json')
+            {"diaryTitle": "Wed", "date": "2/5/2018", "diaryBody": ""}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        self.assertIn('date is required', str(response.data))
+        self.assertIn('Field required: Please write someting',str(response.data))
